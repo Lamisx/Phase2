6bbdccb3a67c0329335cc331ac21ae954be5e034
Lamisx <lamprog01@gmail.com>
2026-02-26 22:24:06 +0300

    Refactor settings to use environment variables and update .gitignore

--- waqaa/BackEnd/core/settings.py
+++ waqaa/BackEnd/core/settings.py
@@ -9,21 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8_m&9-fxy3@nh6bt59fbkom)@8&nd6_e7)k*6e1d61q#q$bn=g'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-dev-key")
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
