06ad1f8c0e27c47e91076394bc39ab38bc80ddd9
Rawan <rawanalshmsam@gmail.com>
2026-03-10 11:06:16 +0300

    Add Django environment variables to models.py
    Added environment variables for Django configuration.

--- waqaa/BackEnd/devices/models.py
+++ waqaa/BackEnd/devices/models.py
@@ -9,7 +9,15 @@
 
 
 
+#هذا يرفع 
+DJANGO_SECRET_KEY=waqa-super-secret-key-change-this-later
+DJANGO_DEBUG=True
 
+DB_NAME=postgres
+DB_USER=postgres
+DB_PASSWORD=RWaqaa090@@
+DB_HOST=db.nbxjvaduhnuicamrocnd.supabase.co
+DB_PORT=5432
 
 
 
