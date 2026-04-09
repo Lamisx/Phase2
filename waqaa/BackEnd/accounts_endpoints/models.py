182d1fc1032910be7844522f3391702832f62687
Lamisx <lamprog01@gmail.com>
2026-04-09 16:46:34 +0300

    acc-mod-1

--- waqaa/BackEnd/accounts_endpoints/models.py
+++ waqaa/BackEnd/accounts_endpoints/models.py
@@ -6,15 +6,15 @@ class WaqaUser(models.Model):
     STATUS_CHOICES = [('active', 'Active'), ('suspended', 'Suspended'), ('deleted', 'Deleted')]
 
     id               = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username         = models.TextField(null=False, blank=True, unique=True)
-    display_name     = models.TextField(null=False, blank=True)
+    username         = models.TextField(null=False, blank=False, unique=True)
+    display_name     = models.TextField(null=False, blank=False)
     email            = models.TextField(null=True, blank=True,unique=True)
-    phone            = models.TextField(null=False, blank=True,unique=True)
-    national_id_hmac = models.TextField(null=False, blank=True,unique=True)
+    phone            = models.TextField(null=False, blank=False,unique=True)
+    national_id_hmac = models.TextField(null=False, blank=False,unique=True)
     status           = models.TextField(default='active', choices=STATUS_CHOICES)
     created_at       = models.DateTimeField(auto_now_add=True)
     updated_at       = models.DateTimeField(auto_now=True)
-    password_hash = models.TextField(null=False, blank=True)
+    password_hash = models.TextField(null=False, blank=False)
 
     class Meta:
         db_table = 'users'
