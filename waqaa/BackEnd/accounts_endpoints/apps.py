2acf184ab6f523b2829a23005947d803d8286628
Lamisx <lamprog01@gmail.com>
2026-05-15 17:10:59 +0300

    Remove unused migration files and clean up app configuration

--- waqaa/BackEnd/accounts_endpoints/apps.py
+++ waqaa/BackEnd/accounts_endpoints/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class AccountsEndpointsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts_endpoints"
-    label = "account"
